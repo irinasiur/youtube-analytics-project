9213875472c76fb606e352ad8ed1039f56a8074b
irinasiur <drozdovairina10@gmail.com>
2023-05-30 20:19:09 +0300

    good     def print_info(self) -> None:

--- src/channel.py
+++ src/channel.py
@@ -4,14 +4,6 @@ import os
 
 from googleapiclient.discovery import build
 
-# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-api_key: str = os.getenv('YT_API_KEY')
-
-# создать специальный объект для работы с API
-youtube = build('youtube', 'v3', developerKey=api_key)
-
-#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/irinka/.config/gcloud/application_default_credentials.json"
-
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -22,9 +14,8 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,contentDetails,statistics,status').execute()
-        print(type(channel))
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        dict_to_print = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
-    # def printj(dict_to_print: dict) -> None:
-    #     """Выводит словарь в json-подобном удобном формате с отступами"""
-    #     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
