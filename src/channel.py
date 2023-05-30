ac595d273c4e8c8619f4a76e8943a2c9a8cb0f68
irinasiur <drozdovairina10@gmail.com>
2023-05-30 13:34:32 +0300

    ImportError: cannot import name 'TBufferedTransport' from '_shaded_thriftpy.transport.buffered' (unknown location)

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,16 @@
+import http
+import json
+import os
+
+from googleapiclient.discovery import build
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/irinka/.config/gcloud/application_default_credentials.json"
 
 
 class Channel:
@@ -9,4 +22,9 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(self.channel_id)
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,contentDetails,statistics,status').execute()
+        print(type(channel))
+
+    # def printj(dict_to_print: dict) -> None:
+    #     """Выводит словарь в json-подобном удобном формате с отступами"""
+    #     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
